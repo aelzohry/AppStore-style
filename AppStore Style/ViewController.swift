7fdcc9ac76061f91d6119d65382ee3a113fd280d
Ahmed Elzohry <ahmed@ahmeds-macbook-pro.local>
2016-07-29 02:20:45 +0200

    Initial Commit

---
+++ AppStore Style/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppStore Style
+//
+//  Created by Ahmed Elzohry on 7/29/16.
+//  Copyright © 2016 Ahmed Elzohry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
